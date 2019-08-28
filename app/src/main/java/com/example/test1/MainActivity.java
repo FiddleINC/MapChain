1e55ffe6383fa706c2df24baaae6f617938f66f7
Prachurjya Biswas <biswasprachurjya@gmail.com>
2019-08-28 11:15:41 +0530

    Added MiniMap

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
@@ -15,19 +12,21 @@ import org.osmdroid.tileprovider.MapTileProviderBasic;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MinimapOverlay;
 import org.osmdroid.views.overlay.TilesOverlay;
 import org.osmdroid.views.overlay.compass.CompassOverlay;
 import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
-
-import static java.security.AccessController.getContext;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 
 public class MainActivity extends AppCompatActivity {
 
     MapView map = null;
-    private CompassOverlay mCompassOverlay;
+    //private CompassOverlay mCompassOverlay;
 
-    @Override public void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //handle permissions first, before map is created. not depicted here
@@ -38,25 +37,37 @@ public class MainActivity extends AppCompatActivity {
         //inflate and create the map
         setContentView(R.layout.activity_main);
 
-        map = findViewById(R.id.map);
+        map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setMultiTouchControls(true);
         IMapController mapController = map.getController();
         GeoPoint startPoint = new GeoPoint(22.308208, 87.281945);
+        mapController.setZoom(7);
         mapController.setCenter(startPoint);
+
+        MinimapOverlay mMiniMap = new MinimapOverlay(this, map.getTileRequestCompleteHandler());
+        map.getOverlays().add(mMiniMap);
 
         MapTileProviderBasic mProvider = new MapTileProviderBasic(getApplicationContext());
         mProvider.setTileSource(TileSourceFactory.FIETS_OVERLAY_NL);
         TilesOverlay mTilesOverlay = new TilesOverlay(mProvider, this.getBaseContext());
         map.getOverlays().add(mTilesOverlay);
+
+        MyLocationNewOverlay mLocationOVerlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getApplicationContext()), map);
+        mLocationOVerlay.enableMyLocation();
+        map.getOverlays().add(mLocationOVerlay);
+
+        CompassOverlay mCompassOverlay = new CompassOverlay(getApplicationContext(), new InternalCompassOrientationProvider(getApplicationContext()), map);
+        mCompassOverlay.enableCompass();
+        map.getOverlays().add(mCompassOverlay);
     }
 
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
     }
 
-    public void onPause(){
+    public void onPause() {
         super.onPause();
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
