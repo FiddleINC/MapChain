4b82e8fb966c686c10cf3f65cad2f13810a6ce3a
Prachurjya Biswas <biswasprachurjya@gmail.com>
2019-09-06 20:37:09 +0530

    Added DB Class

--- app/src/main/java/com/example/test1/DBHelper.java
+++ app/src/main/java/com/example/test1/DBHelper.java
@@ -6,23 +6,30 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "";
+    private static final String DATABASE_NAME = "Mapchain";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "MapData";
+    private static final String COLUMN_LATITUDE = "Latitude";
+    private static final String COLUMN_LONGITUDE= "Longitude";
+    private static final String COLUMN_GEOHASH = "Geohash";
 
-    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String sql = "CREATE TABLE " + TABLE_NAME + " ("  + COLUMN_LATITUDE + " double NOT NULL," +  COLUMN_LONGITUDE + " double NOT NULL," + COLUMN_GEOHASH + " varchar(200) NOT NULL" + ");";
+        sqLiteDatabase.execSQL(sql);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        String sql = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
     }
 }
