277b6c246eaae8b88fb1b471794e638bfc46b55e
Shourya Kumar <skshourya@gmail.com>
2019-11-11 21:17:25 +0530

    added polygon

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import geohasher.GeoHasher;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 import android.content.Context;
 import android.graphics.Color;
-import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.widget.Toast;
 
 import com.github.davidmoten.geo.GeoHash;
 
@@ -27,6 +35,7 @@ import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,13 +48,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        System.out.println("geohash:" );
         //handle permissions first, before map is created. not depicted here
         Context ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
 
         //inflate and create the map
         setContentView(R.layout.activity_main);
+
 
         map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
@@ -78,11 +88,49 @@ public class MainActivity extends AppCompatActivity {
         startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         map.getOverlays().add(startMarker);
 
-        map.getOverlayManager().add(makePolygon());
+        ImageButton button = (ImageButton) findViewById(R.id.imageButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(),"Make Polygon", Toast.LENGTH_SHORT).show();
+                map.getOverlays().add(new MapEventsOverlay(getPoints()));
+                //map.getOverlayManager().add(makePolygon(NULL));
+            }
+        });
 
+        ImageButton button1 = (ImageButton) findViewById(R.id.clickButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(),"Polygon Created", Toast.LENGTH_SHORT).show();
+                map.getOverlays().add(new MapEventsOverlay(getPoints()));
+                //map.getOverlayManager().add(makePolygon(NULL));
+            }
+        });
     }
 
-    public Polygon makePolygon(GeoPoint... points) {
+    public MapEventsReceiver getPoints() {
+        MapEventsReceiver mReceive = new MapEventsReceiver(){
+            @Override
+            public boolean singleTapConfirmedHelper(GeoPoint p) {
+                Marker startMarker = new Marker(map);
+                startMarker.setPosition(p);
+                startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                map.getOverlays().add(startMarker);
+                return true;
+            }
+
+            @Override
+            public boolean longPressHelper(GeoPoint p) {
+                List<GeoPoint> points = new ArrayList<>();
+                return false;
+            }
+        };
+        return mReceive;
+    }
+
+
+    public Polygon makePolygon(List<GeoPoint> points) {
         List<GeoPoint> geoPoints = new ArrayList<>();
         for (GeoPoint p : points) {
             geoPoints.add(p);
@@ -113,13 +161,16 @@ public class MainActivity extends AppCompatActivity {
                 double longt = p.getLongitude();
                 int length = 9;
                 String geohash = GeoHash.encodeHash(lat, longt, length);
+                System.out.println("geohash:" + geohash);
                 DBHelper mDatabase = new DBHelper(getBaseContext(),lat, longt, geohash);
                 if (mDatabase.insertData())
                     Toast.makeText(getBaseContext(),"Added", Toast.LENGTH_SHORT).show();
                 else
+                    //Toast.makeText(getBaseContext(), Toast.LENGTH_SHORT).show();
                     Toast.makeText(getBaseContext(),"Not Added", Toast.LENGTH_SHORT).show();
-                return true;
-            }
+
+                    return true;
+                }
 
             @Override
             public boolean longPressHelper(GeoPoint p) {
